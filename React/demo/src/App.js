f7431fdf3ef254209a60970ba704b6154d34519b
Shubham-2122 <shubhamjadav.tops@gmail.com>
2024-12-27 10:53:01 +0530

    compo

--- React/demo/src/App.js
+++ React/demo/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Class_compo from "./Component/Class-compo";
+import Fun_compo from "./Component/Fun_compo";
 
 function App() {
   return (
-    <h1>Hello this app page</h1>
+    <div>
+      <h1>Hello this app page</h1>
+      <Class_compo />
+      <Fun_compo />
+    </div>
   );
 }
 
