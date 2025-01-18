055a5a27eb2341e7492b581e634af8495906e520
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-01-18 11:11:53 +0530

    font-backgroud-text

--- React/react-demo/src/App.jsx
+++ React/react-demo/src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import Font from "./Font"
 
 function App() {
 
   return (
-   <h1 className="bg-green-500 text-6xl text-white">hello this vite project</h1>
+   <>
+    <h1 className="bg-green-500 text-6xl text-white">hello this vite project</h1>
+    <Font />
+   </>
   )
 }
 
